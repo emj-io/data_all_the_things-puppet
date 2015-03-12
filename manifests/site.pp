88c0a71382f6b80bd7797e9adee7340d29a62575
Ubuntu <eric.johnson@mycase.com>
2015-03-12 23:32:10 +0000

    emj - directions to install influxdb

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,14 @@
 node default {
+#Prereqs
+##Influxdb:
+### http://influxdb.com/download/
+### wget https://s3.amazonaws.com/influxdb/influxdb_latest_amd64.deb
+### sudo dpkg -i influxdb_latest_amd64.deb
+  
   package { 'clojure1.4':
     ensure => installed
   }
   package { 'puppet-el':
     ensure => installed
   }	    		    
-  
 }
