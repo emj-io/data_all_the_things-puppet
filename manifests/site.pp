aec15ce8207aef348c36dda6c2ed28b159b10d07
Ubuntu <eric.johnson@mycase.com>
2015-03-12 23:35:25 +0000

    emj - directions to install riemann.io

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,6 +4,9 @@ node default {
 ### http://influxdb.com/download/
 ### wget https://s3.amazonaws.com/influxdb/influxdb_latest_amd64.deb
 ### sudo dpkg -i influxdb_latest_amd64.deb
+## Riemann.io
+### wget https://aphyr.com/riemann/riemann_0.2.9_all.deb
+### dpkg -i wget riemann_0.2.9_all.deb
   
   package { 'clojure1.4':
     ensure => installed
